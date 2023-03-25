9eac3fd7f65b84643d2de88c6b743c59f395a2a8
Sergio Iglesias <SergioIglesias@pc-139-125.customer.ask4.lan>
2023-03-25 20:28:48 +0100

    feat: landig page and user fixes

---
+++ src/views/Landing/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FC, useCallback, useState } from 'react';
+import { syncCategories } from '../../services/api/selection';
+import {
+  ButtonBoxes,
+  SyncButton,
+  WelcomeButton,
+  WelcomeContainer,
+  WelcomeMessage,
+} from './styles';
+
+const Landing: FC = () => {
+  const [isloading, setIsLoading] = useState<boolean>(false);
+  const syncData = useCallback(async () => {
+    await syncCategories();
+    setIsLoading(false);
+  }, []);
+  if (isloading) {
+    return <h1>LOADING</h1>;
+  }
+  return (
+    <WelcomeContainer>
+      <SyncButton onClick={syncData}>Sync Categories</SyncButton>
+      <WelcomeMessage>Select the desired info</WelcomeMessage>
+      <ButtonBoxes>
+        <WelcomeButton to="/categories">Categories</WelcomeButton>
+        <WelcomeButton to="/random">Get Random Cocktail!</WelcomeButton>
+        <WelcomeButton to="/creation">Post your cocktail</WelcomeButton>
+        <WelcomeButton to="/post">All Post</WelcomeButton>
+      </ButtonBoxes>
+    </WelcomeContainer>
+  );
+};
+
+export default Landing;
