f5ffe7bf9b166c679cb4df623e1b13a22972648b
Sergio Iglesias <SergioIglesias@pc-139-125.customer.ask4.lan>
2023-03-27 18:21:36 +0200

    feat: movies call logic

---
+++ src/models/movies.ts
@@ -0,0 +1,28 @@
+import { MovieResponse } from "../services/api/movies";
+
+export type MovieInput = {
+  id: string;
+  created: number;
+  title: string;
+  rating: string;
+  critics:number;
+  byLIne:string;
+  createdAt: Date;
+  updatedAt: Date;
+  link: string
+};
+
+export const normalizeMovie = (input: MovieResponse) => {
+  return {
+    id: input?.id || "",
+    title: input?.title || "",
+    rating: input?.rating || "",
+    critics: input?.critics || Number(),
+    byLine: input?.byLine || "",
+    link: input?.link || "",
+    created: input?.created || Date(),
+    updated: input?.updated || Date(),
+  };
+};
+
+export type Movie = ReturnType<typeof normalizeMovie>;
---
+++ src/services/api/movies.ts
@@ -0,0 +1,39 @@
+import { getToken } from "../api/storage";
+import { Movie, normalizeMovie } from "../../models/movies";
+
+
+export type MovieResponse = {
+    id: string;
+    created: string;
+ title: string;
+  rating: string;
+  critics: number;
+ byLine: string;
+ publication: Date;
+  updated: Date;
+  link: string;
+
+};
+
+
+const BASE_API_URL="http://localhost:8000/movies"
+
+export const getCategoryDrinksById = async (
+  id: string
+): Promise<Movie[]> => {
+  try {
+    const token = getToken();
+    const response = await fetch(`${BASE_API_URL}/${id}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data: MovieResponse[] = await response.json();
+    console.log({ data });
+    return data.map(normalizeMovie);
+  } catch (error) {
+    console.log((error as Error).message);
+  }
+  return [];
+};
+
+
